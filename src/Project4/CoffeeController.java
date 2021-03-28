73e7c5a26642465c24e2aa878ba815b30f8a53b7
Catherine Nguyen <catherinevn.11@gmail.com>
2021-03-28 17:47:31 -0400

    coffee fxml

--- src/Project4/CoffeeController.java
+++ src/Project4/CoffeeController.java
@@ -1,8 +1,22 @@
 package Project4;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Spinner;
 
 public class CoffeeController {
+
+    @FXML
+    private RadioButton choiceShort, choiceTall, choiceGrande, choiceVenti;
+
+    @FXML
+    private ToggleGroup size;
 
     @FXML
     private MainMenuController mainMenuController;
