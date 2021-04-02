ed316a0630976ccbacbbc8f814f7fc49d1a051cb
Catherine Nguyen <catherinevn.11@gmail.com>
2021-04-02 11:01:06 -0400

    donut and menu item documentation

--- src/Project4/Donut.java
+++ src/Project4/Donut.java
@@ -1,5 +1,11 @@
 package Project4;
 
+/**
+ * This class represents a Donut item.
+ * It extends the MenuItem class, and also includes fields for the type of donut, flavor and quantity.
+ * @author Srija Gottiparthi, Catherine Nguyen
+ */
+
 public class Donut extends MenuItem {
     private String type; // yeast, cake, or donut hole
     private String flavor; // the flavor
@@ -10,6 +16,12 @@ public class Donut extends MenuItem {
     private final static double CAKE_PRICE = 1.59;
     private final static double HOLE_PRICE = 0.33;
 
+    /**
+     * Initializes a new Donut object with with a given type, flavor and quantity.
+     * @param type type of donut
+     * @param flavor donut flavor
+     * @param quantity amount of donuts
+     */
     public Donut(String type, String flavor, int quantity) {
         super();
         this.type = type;
@@ -17,6 +29,9 @@ public class Donut extends MenuItem {
         this.quantity = quantity;
     }
 
+    /**
+     * Calculates the price of a donut.
+     */
     public void itemPrice() {
         if (type.equals("Yeast Donut")) {
             price = YEAST_PRICE;
@@ -31,10 +46,18 @@ public class Donut extends MenuItem {
         price *= quantity;
     }
 
+    /**
+     * Returns the price of the donut.
+     * @return donut price
+     */
     public double getPrice() {
         return price;
     }
 
+    /**
+     * Returns a string representation of the donut
+     * @return string representation of the donut
+     */
     @Override
     public String toString() {
         return (type + "(" + quantity + "), " + flavor);
