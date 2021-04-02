e0f72f9feec77252568cfddb4ecf1670b5bd498c
Catherine Nguyen <catherinevn.11@gmail.com>
2021-04-02 10:49:01 -0400

    doc for order.java

--- src/Project4/Order.java
+++ src/Project4/Order.java
@@ -1,4 +1,9 @@
 package Project4;
+/**
+ * This class represents an order in the cafe.
+ * An order includes fields such as the list of menu items, unique order number and subtotal.
+ * @author Srija Gottiparthi, Catherine Nguyen
+ */
 
 import java.util.ArrayList;
 
@@ -8,29 +13,53 @@ public class Order implements Customizable {
     private double subtotal;
     private static final double NJ_SALES_TAX_RATE = 0.06625;
 
+    /**
+     * Initializes a new Order object with an empty array size.
+     */
     public Order() {
         order = new ArrayList<MenuItem>();
     }
 
+    /**
+     * Initializes a new Order with an empty array size and order number.
+     * @param orderNumber the number of the order
+     */
     public Order(int orderNumber) {
         order = new ArrayList<MenuItem>();
         this.orderNumber = orderNumber;
     }
 
+    /**
+     * Initializes a new Order with given order details such as order number and subtotal.
+     * @param o
+     */
     public Order (Order o) {
         this.order = o.order;
         this.orderNumber = o.orderNumber;
         this.subtotal = o.subtotal;
     }
 
+    /**
+     * Set the order's number.
+     * @param orderNumber number of the order
+     */
     public void setOrderNumber(int orderNumber) {
         this.orderNumber = orderNumber;
     }
 
+    /**
+     * Returns the order number.
+     * @return order number
+     */
     public int getOrderNumber() {
         return orderNumber;
     }
 
+    /**
+     * Adds either a Coffee or Donut object to the order.
+     * @param obj the object to add
+     * @return true if object is successfully added, false otherwise
+     */
     @Override
     public boolean add(Object obj) {
         if (obj instanceof Coffee) {
@@ -50,6 +79,11 @@ public class Order implements Customizable {
         return false;
     }
 
+    /**
+     * Removes either a Coffee or a Donut object from the order
+     * @param obj the object to remove
+     * @return true if object is successfully removed, false otherwise
+     */
     @Override
     public boolean remove(Object obj) {
         if (obj instanceof Coffee) {
@@ -69,22 +103,42 @@ public class Order implements Customizable {
         return false;
     }
 
+    /**
+     * Returns the order
+     * @return order
+     */
     public ArrayList<MenuItem> getOrder() {
         return order;
     }
 
+    /**
+     * Returns the subtotal of the order
+     * @return the subtotal
+     */
     public double getSubtotal() {
         return subtotal;
     }
 
+    /**
+     * Returns the tax of the order
+     * @return tax
+     */
     public double getTax() {
         return subtotal * NJ_SALES_TAX_RATE;
     }
 
+    /**
+     * Returns the total of the order
+     * @return total
+     */
     public double getTotal() {
         return subtotal + getTax();
     }
 
+    /**
+     * Returns a string representation of the order
+     * @return string representation of order
+     */
     @Override
     public String toString() {
         String orderDetails = "Order #" + orderNumber + ": ";
